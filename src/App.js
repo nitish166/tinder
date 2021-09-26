1fb02a4b57a00f9e27f7cef5ec8499f8effc958f
unknown <knitish123@gmail.com>
2021-09-26 13:08:45 +0530

    Header component completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Im header component</h1>
+      {/* Header */}
+      <Header />
+      {/* TinderCard  */}
+      {/* SwipeButton  */}
     </div>
   );
 }
